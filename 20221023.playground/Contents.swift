170b55e42b90f03eeb2d5e6cbd49927277194bfb
Hangga99 <qkrrlxo418@naver.com>
2022-10-23 02:57:59 +0900

    add

---
+++ 20221023.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+import Foundation
+
+// 나머지 구하기
+//import Foundation
+//
+//func solution(_ num1:Int, _ num2:Int) -> Int {
+//    return num1 % num2
+//}
+
+// 중앙값 구하기
+//func solution(_ array:[Int]) -> Int {
+//    return array.sorted()[array.count / 2]
+//}
+
+// 짝수는 싫어요
+//func solution(_ n:Int) -> [Int] {
+//    var nums: [Int] = []
+//    for i in 1...n {
+//        if i % 2 == 1 {
+//            nums.append(i)
+//        }
+//    }
+//    return nums
+//}
